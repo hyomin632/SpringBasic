72324e1ebb1f210ef2650428f325e9e35c4b181b
hyomin632 <80568982+hyomin632@users.noreply.github.com>
2021-05-28 17:36:10 +0900

    ioc, di 기본

---
+++ celeste/spring/beans/HelloSpring5Bean01.java
@@ -0,0 +1,9 @@
+package celeste.spring.beans;
+
+public class HelloSpring5Bean01 {
+
+	public void sayHello(String msg) {
+		System.out.println("안녕하세요, " + msg);
+	}
+
+}
---
+++ celeste/spring/beans/HelloSpring5Bean02.java
@@ -0,0 +1,13 @@
+package celeste.spring.beans;
+
+public class HelloSpring5Bean02 {
+
+	public void sayHellokor(String msg) {
+		System.out.println("안녕하세요, " + msg);
+	}
+	
+	public void sayHelloeng(String msg) {
+		System.out.println("Hello, " + msg);
+	}
+
+}
